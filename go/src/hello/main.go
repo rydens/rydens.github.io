b4e26fac4d6156d0f9c10398bd990550841acae7
rydens <rdensmore365@gmail.com>
2016-07-14 20:41:42 -0400

    save

--- go/src/hello/main.go
+++ go/src/hello/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	c "github.com/skilstak/go-colors"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println(c.Green + "Hello, World!")
+	fmt.Println(c.Cyan + "I am a computer!")
+	fmt.Println(c.Red + "I am fast!" + c.X)
 }
